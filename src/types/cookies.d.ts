24b12812fdb5dfd8c35b17ab923af714d5e82bfc
samdahboss <samuelolawumi65@gmail.com>
2025-02-08 14:33:50 +0100

    Add cookie dependency and define types for cookie parsing and serialization

--- src/types/cookies.d.ts
+++ src/types/cookies.d.ts
@@ -1,5 +1,26 @@
 declare module "cookie" {
-    export function parse(str: string, options?: any): Record<string, string>;
-    export function serialize(name: string, value: string, options?: any): string;
+  interface CookieParseOptions {
+    decode?: (value: string) => string;
   }
-  
+
+  interface CookieSerializeOptions {
+    domain?: string;
+    encode?: (value: string) => string;
+    expires?: Date;
+    httpOnly?: boolean;
+    maxAge?: number;
+    path?: string;
+    sameSite?: boolean | "lax" | "strict" | "none";
+    secure?: boolean;
+  }
+
+  export function parse(
+    str: string,
+    options?: CookieParseOptions
+  ): Record<string, string>;
+  export function serialize(
+    name: string,
+    value: string,
+    options?: CookieSerializeOptions
+  ): string;
+}

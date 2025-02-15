976f12914311a0d73fbbe78970954f2dd010ea28
samdahboss <samuelolawumi65@gmail.com>
2025-02-15 15:50:01 +0100

    Enhance CollectionPage with navigation link to Favourites and update CollectionDetailsSection layout

--- src/app/favourites/[collectionId]/page.tsx
+++ src/app/favourites/[collectionId]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useParams } from "next/navigation";
 import { collections } from "@/app/utils/FavoriteCollection";
 import { notFound } from "next/navigation";
@@ -6,9 +6,12 @@ import CollectionDetailsSection from "@/app/ui/favorites/CollectionDetailsSectio
 import { useUser } from "@/app/context/UserContext";
 import Navbar from "@/app/ui/Navbar";
 import Footer from "@/app/ui/components/Footer";
+import Link from "next/link";
+import { cabin } from "@/app/ui/fonts";
+import {  FaChevronLeft } from "react-icons/fa6";
 
 const CollectionPage = () => {
-  const {user} = useUser();
+  const { user } = useUser();
   const params = useParams(); // Get route params
   const collectionId = params?.collectionId as string; // Cast collectionId to string
 
@@ -17,20 +20,25 @@ const CollectionPage = () => {
 
   if (!collection) return notFound(); // Show 404 if not found
 
-  
   return (
-    <div className="">
-      <div className="text-[#8B3A2D]">
-        <Navbar user={user} theme="dark"/>
+    <div className=''>
+      <div className='text-[#8B3A2D]'>
+        <Navbar user={user} theme='dark' />
       </div>
-      
+
       <div className='relative container mx-auto px-6 lg:px-24 w-full'>
-        <CollectionDetailsSection collection={collection}/>
+        <Link href='/favourites' passHref>
+          <div className={`${cabin.className} text-[16px] mt-32 px-6 flex items-center`}>
+            <FaChevronLeft  />
+
+            <h2 className=''>Favorites</h2>
+          </div>
+        </Link>
+        <CollectionDetailsSection collection={collection} />
       </div>
 
       <Footer />
     </div>
-    
   );
 };
 
--- src/app/ui/favorites/CollectionDetailsSection.tsx
+++ src/app/ui/favorites/CollectionDetailsSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import PropertyCard from "../home/PropertyCard";
 
 interface CollectionDetailsProp {
   collection: {
@@ -23,19 +23,12 @@ export default function CollectionDetailsSection({
   collection,
 }: CollectionDetailsProp) {
   return (
-    <section className='min-h-screen mt-32 p-6'>
-      <h2 className='text-3xl font-semibold'>{collection.title}</h2>
+    <section className='min-h-screen mt-8 p-6'>
+      <h2 className='text-2xl font-semibold text-[#8B3A2D]'>{collection.title}</h2>
 
-      <div className='mt-8 grid md:grid-cols-3 gap-6'>
+      <div className='mt-8 grid md:grid-cols-4 gap-6'>
         {collection.collectionProperties.slice(0,4).map((img, index) => (
-          <div key={index} className='relative h-60'>
-            <Image
-              src={img.image}
-              alt={collection.title}
-              fill
-              className='rounded-md'
-            />
-          </div>
+          <PropertyCard key={index} {...img} />
         ))}
       </div>
     </section>

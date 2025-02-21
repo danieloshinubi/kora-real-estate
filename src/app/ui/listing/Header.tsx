ab7627fa14dd1f1f5fe674e7c7c78d7c8a3d9484
samdahboss <samuelolawumi65@gmail.com>
2025-02-21 10:38:54 +0100

    Enhance CollectionCard hover effect and update Header component to use star icons for rating display

--- src/app/ui/listing/Header.tsx
+++ src/app/ui/listing/Header.tsx
@@ -1,13 +1,18 @@
+import { FaStar } from "react-icons/fa6";
 import { cabin } from "../fonts";
 
 export default function Header() {
-    return (
-      <div className="">
-        <h1 className="text-3xl text-[#8B3A2D] font-semibold">
-          Relaxing retreat at the heart of Yuba City.
-        </h1>
-        <p className="text-yellow-500 mt-2">⭐⭐⭐⭐⭐<span className={`${cabin.className} text-black`}> 5 star</span></p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="">
+      <h1 className="text-3xl text-[#8B3A2D] font-semibold">
+        Relaxing retreat at the heart of Yuba City.
+      </h1>
+      <p className="text-yellow-500 mt-2">
+        {[...Array(5)].map((_, i) => (
+          <FaStar key={i} />
+        ))}
+        <span className={`${cabin.className} text-black`}> 5 star</span>
+      </p>
+    </div>
+  );
+}

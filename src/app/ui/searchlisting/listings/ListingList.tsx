58299e06910e12b0790e41f0b675e4b9c1305b5b
samdahboss <samuelolawumi65@gmail.com>
2025-02-27 21:42:27 +0100

    Update ListingList component to use flexbox for improved layout and spacing

--- src/app/ui/searchlisting/listings/ListingList.tsx
+++ src/app/ui/searchlisting/listings/ListingList.tsx
@@ -10,7 +10,7 @@ interface ListingListProps {
 
 const ListingList: React.FC<ListingListProps> = ({ listings }) => {
   return (
-    <div className="space-y-4 overflow-y-auto h-[1100px]">
+    <div className="flex flex-col gap-y-4 space-y-4 overflow-y-auto h-[1100px]">
       {listings.map((listing) => (
         <ListingCard key={listing._id} listing={listing} />
       ))}
